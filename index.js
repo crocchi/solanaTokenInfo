de08eb99af41eb6bf4715bfb8a61fa0a29ef26a6
crocchi <ziocro@gmail.com>
2025-03-07 16:20:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], 
-    requireHeader: ['origin', 'x-requested-with'],
+   // requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2'],
     handleInitialRequest: function(req, res, url) {
         if(req.url.includes('/cro')){
