abfea63b9d32c5a76af6f50ba6ae56bb302d929d
crocchi <ziocro@gmail.com>
2025-03-07 20:09:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     removeHeaders: ['cookie', 'cookie2'],
+
     requireHeader: (req) => {
         if(req.url.includes('streamingcommunity')){
             console.log("cors streamingcommunity");
@@ -15,17 +16,8 @@ cors_proxy.createServer({
         }
         console.log('normale request')
         return ['origin', 'x-requested-with'];
-    },
-    setHeaders: function(req, res, proxyReq) {
-        // Set the Referer or Origin header to a specific site
-        proxyReq.setHeader('Referer', 'https://streamingcommunity.lu');
-        proxyReq.setHeader('Origin', 'https://scrapercb01.onrender.com/');
-    },
-    // Remove 'X-Frame-Options' header from the response
-    onHeadersReceived: function(req, res, proxyRes) {
-        delete proxyRes.headers['x-frame-options'];
-        delete proxyRes.headers['X-Frame-Options'];
     }
+
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
